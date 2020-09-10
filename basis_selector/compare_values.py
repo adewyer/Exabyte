25aa5b3fae764bc1c4a2c6df66ab6c4b67f0c06f
Amanda Dewyer <adewyer@umich.edu>
2020-09-10 16:18:38 -0700

    added requirements.txt doc and updated compare method to fix bug

--- basis_selector/compare_values.py
+++ basis_selector/compare_values.py
@@ -40,8 +40,8 @@ def check_property(molecule, prop, threshold, property_dict, ref):
 def print_acceptable_basis(basis, molecule, threshold):
     fi = open('acceptableBasisSets.log', 'w')
     fi.write("Basis sets that fall within {} of the reference for {}:".format(threshold, molecule))
-    for bas in basis:
-        fi.write(basis)
+    for key, value in basis:
+        fi.write(key)
     fi.close()
     return 0
 
